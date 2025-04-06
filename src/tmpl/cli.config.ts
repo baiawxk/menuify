1692f6defadbbccbc496804958c33bea4061722d
baiawxk <baiawxk@qq.com>
2025-04-06 21:19:58 +0800

    upd

---
+++ src/tmpl/cli.config.ts
@@ -0,0 +1,52 @@
+export default {
+  menus: [
+    {
+      name: 'start n8n',
+      value: 'pnpx n8n',
+      type: 'command',
+      options: {
+        cwd: 'E:\\workspaces\\n8n',
+      },
+    },
+    {
+      name: 'start crontab-ui',
+      value: 'pnpx crontab-ui',
+      type: 'command',
+    },
+    {
+      name: 'create vite-app',
+      value: 'npm create vite@latest',
+      type: 'command',
+    },
+    {
+      name: 'create vue-app',
+      value: 'npm create vue@latest',
+      type: 'command',
+    },
+    {
+      name: 'create electron-app',
+      value: 'npm create electron-app@latest',
+      type: 'command',
+    },
+    {
+      name: 'create nuxt-app',
+      value: 'npm create nuxt-app@latest',
+      type: 'command',
+    },
+    {
+      name: 'create react-app',
+      value: 'npm create react-app@latest',
+      type: 'command',
+    },
+    {
+      name: 'create next-app',
+      value: 'npm create next-app@latest',
+      type: 'command',
+    },
+    {
+      name: 'create svelte-app',
+      value: 'npm create svelte@latest',
+      type: 'command',
+    },
+  ],
+}
